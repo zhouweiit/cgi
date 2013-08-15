1d655b49a77a3f3860e5576951163d9f42ce2024
zhouweiit <zhouwei.it@gmail.com>
2013-08-15 20:47:43 +0800

    article dao

--- default/main.c
+++ default/main.c
@@ -4,4 +4,5 @@ void defaultMainHtml(){
     appendBody("<div style='text-align:center; margin-left:auto;'><h3>留言板主页</h3></div>");    
     dlist *list = NULL;
     selectAllArticle(&list);
+    dlistPrint(list);
 }
